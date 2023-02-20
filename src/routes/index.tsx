5b9a6e64bacb96329dc5a7f678b9394a5ebcf38d
nursat <nbisenbaev@kassa24.kz>
2023-02-20 15:21:13 +0600

    change BroserRouter to HashRouter

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import Login from '../views/Login';
 import Fields from '../views/Fields';
@@ -10,13 +10,13 @@ import ProtectedRoute from './ProtectedRoute';
 
 const AppRoutes = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-        <Route path={ROUTES.MAIN} element={<ProtectedRoute><Navigate to={ROUTES.FIELDS} replace /></ProtectedRoute>}/>
+        <Route path={ROUTES.MAIN} element={<Navigate to={ROUTES.FIELDS} />}/>
         <Route path={ROUTES.LOGIN} element={<GuestRoute><Layout><Login /></Layout></GuestRoute>}/>
         <Route path={ROUTES.FIELDS} element={<ProtectedRoute><Layout><Fields /></Layout></ProtectedRoute>}/>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
